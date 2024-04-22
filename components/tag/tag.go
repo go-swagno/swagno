65363b5920bdc8073e390e3a5202fc4c7a670d68
Anıl Şenay <anil.senay@cimri.com>
2024-04-23 00:08:53 +0300

    Add missing fields

--- components/tag/tag.go
+++ components/tag/tag.go
@@ -2,14 +2,37 @@ package tag
 
 // https://swagger.io/specification/v2/#tagObject
 type Tag struct {
+	Name         string        `json:"name"`
+	Description  string        `json:"description"`
+	ExternalDocs *ExternalDocs `json:"externalDocs,omitempty"`
+}
+
+type ExternalDocs struct {
 	Name        string `json:"name"`
 	Description string `json:"description"`
 }
 
+type TagOpts func(*Tag)
+
+func WithExternalDocs(name string, description string) TagOpts {
+	return func(t *Tag) {
+		t.ExternalDocs = &ExternalDocs{
+			Name:        name,
+			Description: description,
+		}
+	}
+}
+
 // New returns a new Tag.
-func New(name string, description string) Tag {
-	return Tag{
+func New(name string, description string, opts ...TagOpts) Tag {
+	t := Tag{
 		Name:        name,
 		Description: description,
 	}
+
+	for _, opt := range opts {
+		opt(&t)
+	}
+
+	return t
 }
